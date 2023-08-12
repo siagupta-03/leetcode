9c9d930332d83b73cae3170479595591f77f4291
siagupta-03 <112824643+siagupta-03@users.noreply.github.com>
2023-08-12 12:43:26 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (61.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,34 +1,38 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
+        int m = obstacleGrid.length ;
         int n = obstacleGrid[0].length;
-        int dp [][]= new int[m][n];
-        for(int row[]:dp){
-            Arrays.fill(row , -1);
-        }
-      return   f(m-1 , n-1, obstacleGrid , dp);
         
+        int dp[][]= new int [m][n];
+        
+        for(int i=0 ; i < m ; i++){
+            for(int j=0 ; j < n ; j++){
+                 if(obstacleGrid[i][j]==1 ) {
+                     dp[i][j]=0;
+                     }
+                else{
+                if(i==0 && j==0 ) {
+                    dp[i][j]= 1;
+                continue;}
+              else{
+                  int up=0;
+                  int left=0;
+                 
+                 if(i>0){
+                     up = dp[i-1][j];}
+                  if(j>0){
+                      left= dp[i][j-1];}
+                  dp[i][j]= up+left;
+                                                
+                                            }
+                }
+            
+                
+                
+            }
+                                            
+        }
+       return dp[m-1][n-1];
         
     }
-    public int f(int i , int j ,int grid[][], int dp[][]){
-        
-       
-      if(grid[0][0]==1 ) return 0;
-   
-        
-      // if( i>0 && j >0 ) return 0;
-        if(i==0 && j==0 ) return 1;
-        
-        if(i < 0 || j < 0 || grid[i][j]==1)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        
-    int up=  f(i-1, j, grid , dp);
-        int left =  f(i, j-1, grid, dp) ;
-     //    int up = i > 0 ? dp[i - 1][j] : 0;
-       //             int left = j > 0 ? dp[i][j - 1] : 0;
-            
-    
-         return dp[i][j] =up+left;
-       
-}
 }
