5a48835a8e288819a35bd17481b561173a2e9aa8
siagupta-03 <112824643+siagupta-03@users.noreply.github.com>
2023-09-07 23:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 38.9 MB (80.13%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n==1)return false;//alice wins
+        if(n==2)return true;
+        for(int i=1; i <=n ; i++){
+            if(n%i==0)return !(divisorGame(n-i));
+        }
+        return false;
+        
+    }
+}
