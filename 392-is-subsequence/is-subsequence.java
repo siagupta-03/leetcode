9034581cde29a9e8dc65bd22ca825cca93cdb0b7
siagupta-03 <112824643+siagupta-03@users.noreply.github.com>
2023-09-08 18:45:12 +0530

    Time: 6 ms (5.43%) | Memory: 43.5 MB (5.20%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        int dp[][]= new int [s.length()][t.length()];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        if(f(s.length()-1 , t.length()-1, s, t, dp)== s.length()){
+        return true;}
+        return false;
+        
+    }
+    public int f (int i1, int i2, String s , String t , int dp[][]){
+        if(i1<0 || i2<0)return 0;
+
+
+
+
+        if(dp[i1][i2]!=-1)return dp[i1][i2];
+
+
+
+        if(s.charAt(i1)==t.charAt(i2)){
+          dp[i1][i2]= 1+ f(i1-1, i2-1 , s , t , dp);
+
+        }
+        else if (s.charAt(i1)!=t.charAt(i2) ){
+             dp[i1][i2]= Math.max(f(i1-1, i2, s, t, dp) , f(i1, i2-1 , s , t ,dp));
+        }
+        return dp[i1][i2];
+        
+    }
+}
