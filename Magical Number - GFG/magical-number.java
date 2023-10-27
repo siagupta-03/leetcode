fe32f492c97d87150c1ec55cbda9788e24efe721
siagupta-03 <112824643+siagupta-03@users.noreply.github.com>
2023-10-28 02:02:35 +0530

    Added solution - LeetHub

---
+++ Magical Number - GFG/magical-number.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Magic
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+                  arr[i] = sc.nextInt();
+            
+            GfG gfg = new GfG();  
+            System.out.println(gfg.binarySearch(arr, 0, n-1));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class GfG
+{
+    public static int binarySearch(int arr[], int low, int high)
+    {
+        low=0;
+        high=arr.length-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(arr[mid]==mid){
+                return mid;
+            }
+            else if(arr[mid]<mid){
+                low= mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
