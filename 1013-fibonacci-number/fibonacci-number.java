a46588a8a27862d670d1ebd6272c7f1e5177812f
siagupta-03 <112824643+siagupta-03@users.noreply.github.com>
2024-07-31 12:44:05 +0530

    Time: 37 ms (5.32%) | Memory: 44.3 MB (5.72%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int fib(int n) {
-        
+        //top down approach ---momoizations
         int dp[]= new int[n+1];
         Arrays.fill(dp ,-1);
         if(n<=1){
